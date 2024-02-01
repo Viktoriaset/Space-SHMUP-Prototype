9814dae7d96999880d75415e4407f3a606bb3bf6
ViktoriTanos <99451694+Viktoriaset@users.noreply.github.com>
2024-02-02 01:14:02 +1000

    add Enemy_3

--- Assets/__Scripts/Enemy_2.cs
+++ Assets/__Scripts/Enemy_2.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_2 : MonoBehaviour
+public class Enemy_2 : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Set in inspector: Enemy_2")]
+    public float sinEccentricity = 0.6f;
+    public float lifeTime = 10;
+
+    [Header("Set dynamically: Enemy_2")]
+    public Vector3 p0;
+    public Vector3 p1;
+    public float birthTime;
+
+    private void Start()
     {
-        
+        // point on left border
+        p0 = Vector3.zero;
+        p0.x = -bndCheck.camWidth - bndCheck.radius;
+        p0.y = Random.Range(-bndCheck.camHeight, bndCheck.camHeight);
+
+        // point on right border 
+        p1 = Vector3.zero;
+        p1.x = bndCheck.camWidth + bndCheck.radius;
+        p1.y = Random.Range(-bndCheck.camHeight, bndCheck.camHeight);
+
+        if (Random.value > 0.5f)
+        {
+            p0.x *= -1;
+            p1.x *= -1;
+        }
+
+        birthTime = Time.time;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Move()
     {
-        
+        float u = (Time.time - birthTime) / lifeTime;
+
+        if (u > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        u = u + sinEccentricity * (Mathf.Sin(u * Mathf.PI * 2));
+
+        pos = (1 - u) * p0 + u * p1;
     }
 }
